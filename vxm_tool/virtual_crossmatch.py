20457da85bcad420b8eb23864b663c1c93ed0289
NavchetanKaur <nkaur@tulane.edu>
2018-09-18 12:39:55 -0500

    VXM script updated to include probabilities of US equivalents

--- vxm_tool/virtual_crossmatch.py
+++ vxm_tool/virtual_crossmatch.py
@@ -31,7 +31,7 @@ for row in UNOS_UA_eq_file:
 		ua_ag_eqs = row_split[1:]
 		ua_ag_eqs = list(filter(None, ua_ag_eqs))
 		UA_eq_dict[ua_ag] = ua_ag_eqs
-#print(UA_eq_dict)
+print(UA_eq_dict)
 
 
 def vxm_uags(donorags, candidateags):
@@ -95,6 +95,8 @@ def vxm_gls(donor_gl_string, donor_ethnicity, recipient_UA_list):
 	ag_probs = {}
 	donor_ags = []
 	output = gl_string_ags(donor_gl_string, donor_ethnicity)
+	#print("Output")
+	#print(output)
 	
 	for i in output:
 		ag_list = i[0].split("+")
@@ -104,26 +106,40 @@ def vxm_gls(donor_gl_string, donor_ethnicity, recipient_UA_list):
 			else:
 				ag_probs[j] = i[1]
 	
+	##################################################################################################
+
+	for ag in ag_probs.keys():
+	if ag in UA_dict.keys():
+		p=0
+		for eqs in UA_dict[ag]:
+			if eqs in ag_probs.keys():
+				p += ag_probs[eqs]
+				ag_probs[ag] = p
+	else:
+		continue
+
+	#############################################################################################
+	##print(ag_probs)
 	donor_alleles = vxm_hla.gl_string_alleles_list(donor_gl_string)
 	#print(donor_alleles)
 	donor_allele_freqs = conversion_functions_for_VXM.allele_freq(donor_alleles, donor_ethnicity)
 	#print(donor_allele_freqs)
 
-	for k in ag_probs.keys():
+	for k in ag_probs.keys():        #### gets all the mapped donor antigens as a list
 		donor_ags.append(k)
 
 	UA_list = []
-	for ag in recipient_UA_list:
+	for ag in recipient_UA_list:           
 		if ag in UA_eq_dict.keys():
 			UA_list.append(UA_eq_dict[ag])
 		else:
 			UA_list.append([ag])	
 
 
-	recepient_ags = [item for sublist in UA_list for item in sublist]
+	recepient_ags = [item for sublist in UA_list for item in sublist]            ### makes a list from list of lists
 
 
-	donor_alleles_ags = donor_ags + donor_alleles
+	donor_alleles_ags = donor_ags + donor_alleles 							######## list of donor alleles from GL string and the antigens mapped as per ALLAN
 	
 	for ag in recepient_ags:
 		if ag in donor_alleles_ags:
@@ -173,7 +189,16 @@ def vxm_allele_codes(allele_codes_list, donor_ethnicity, recepient_UA_list):
 			else:
 				ag_probs[j] = i[1]
 	#print(ag_probs)
-
+	
+	for ag in ag_probs.keys():
+	if ag in UA_dict.keys():
+		p=0
+		for eqs in UA_dict[ag]:
+			if eqs in ag_probs.keys():
+				p += ag_probs[eqs]
+				ag_probs[ag] = p
+	else:
+		continue
 	
 	donor_alleles = vxm_hla.allele_code_to_allele_list(allele_codes_list)
 	donor_allele_freqs = conversion_functions_for_VXM.allele_freq(donor_alleles, donor_ethnicity)
